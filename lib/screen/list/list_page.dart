b86a11e180e53cef37d2de0581ae5dee4b17131d
elshodatc111 <elshodatc1116@gmail.com>
2024-10-18 22:23:16 +0500

    Save

--- lib/screen/list/list_page.dart
+++ lib/screen/list/list_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
@@ -11,82 +10,113 @@ class ListPage extends StatefulWidget {
 }
 
 class _ListPageState extends State<ListPage> {
+  List<dynamic> records = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchRecords();
+  }
+
+  Future<void> fetchRecords() async {
+    final url = 'https://cyberkarshi.uz/app/figurant_api_v3.php';
+    try {
+      final response = await http.get(Uri.parse(url));
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        setState(() {
+          records = data['records'];
+          print(records);
+        });
+      } else {
+        throw Exception('Failed to load records');
+      }
+    } catch (e) {
+      print("Error fetching records: $e");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 8.0),
-      color: const Color(0xffF0F4F8),
-      child: ListView.builder(
-        itemCount: 15,
-        itemBuilder: (BuildContext context, int index) {
-          return Card(
-            elevation: 4.0,
-            margin: const EdgeInsets.symmetric(vertical: 10.0),
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(15.0),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(
-                    width: 120,
-                    child: Column(
-                      children: [
-                        const Text(
-                          'Rasmiy qidiruv',
-                          style: TextStyle(
-                            color: Colors.redAccent,
-                            fontSize: 12.0,
-                            fontWeight: FontWeight.bold,
+    return Scaffold(
+      body: records.isEmpty
+          ? const Center(child: CircularProgressIndicator())
+          : Container(
+        padding: const EdgeInsets.all(12.0),
+        color: const Color(0xffF0F4F8),
+        child: ListView.builder(
+          itemCount: records.length,
+          itemBuilder: (BuildContext context, int index) {
+            final record = records[index];
+            return Card(
+              elevation: 4.0,
+              margin: const EdgeInsets.symmetric(vertical: 10.0),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15.0),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      width: 120,
+                      child: Column(
+                        children: [
+                          const Text(
+                            'Rasmiy qidiruv',
+                            style: TextStyle(
+                              color: Colors.redAccent,
+                              fontSize: 12.0,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
-                        ),
-                        const SizedBox(height: 10.0),
-                        ClipRRect(
-                          borderRadius: BorderRadius.circular(10.0),
-                          child: Image.network(
-                            'https://th.bing.com/th/id/OIP.WTZEWYDke1wK-Q8gN7tu0QHaIy?w=863&h=1024&rs=1&pid=ImgDetMain',
-                            fit: BoxFit.cover,
-                            height: 150,
+                          const SizedBox(height: 10.0),
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(10.0),
+                            child: Image.network(
+                              record['photo'] != null && record['photo'] != "/interface/html/"
+                                  ? 'https://cyberkarshi.uz/app${record['photo']}'
+                                  : 'https://th.bing.com/th/id/OIP.5HyjPhedse6Qhpy7IOS8XgHaHa?rs=1&pid=ImgDetMain', // Default rasm URL
+                              fit: BoxFit.cover,
+                              height: 150,
+                            ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
-                  ),
-                  const SizedBox(width: 16),
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Center(
-                          child: Text(
-                            "Qarshi shaxar",
+                    const SizedBox(width: 16),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            record['fio'],
                             style: TextStyle(
                               fontSize: 18,
                               fontWeight: FontWeight.bold,
                               color: Colors.blueGrey.shade800,
                             ),
                           ),
-                        ),
-                        const SizedBox(height: 10),
-                        const InfoRow(title: "FIO:", value: "Toshev Qudrat Murtozqulovich"),
-                        const InfoRow(title: "Tug'ilgan kun:", value: "11.03.2024"),
-                        const InfoRow(title: "Manzil:", value: "Qarshi tuman A.Navoiy ko'chasi"),
-                        const InfoRow(title: "JK moddasi:", value: "167-modda 4-qisim"),
-                        const InfoRow(title: "QYJ vaqti:", value: "№16760 16.06.2021 yil"),
-                      ],
+                          const SizedBox(height: 10),
+                          InfoRow(title: "Tug'ilgan kun:", value: record['birth_date']),
+                          InfoRow(title: "Manzil:", value: record['adress']),
+                          InfoRow(title: "JK moddasi:", value: record['jk_modda']),
+                          InfoRow(title: "QYJ vaqti:", value: record['qyj']),
+                        ],
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-          );
-        },
+            );
+          },
+        ),
       ),
     );
   }
 }
+
 class InfoRow extends StatelessWidget {
   final String title;
   final String value;
